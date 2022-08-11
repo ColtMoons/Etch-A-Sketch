d577061223ddb3df3431dbe4b24854e3bcf1f209
Coltmoons <coltmoonsac@gmail.com>
2022-08-11 15:42:17 -0500

    Add button to create grid base on a size give by user

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-function setDivsGrid(n = 16){
+//calculate square size
+
+function getSquareSize(grid, squares){
+    const gridSize = grid.getBoundingClientRect().height - 4;
+    const squareSize = gridSize / squares;
+    return squareSize;
+}
+
+//create squares for the grid initialy 16 x 16 squares 
+function setDivsGrid(squares = 16){
     const grid = document.querySelector('.grid');
 
-    for(let i = 0; i < n; i++){
-        for(let j = 0; j < n; j++){
+    const squareSize = getSquareSize(grid, squares);
+
+    for(let i = 0; i < squares; i++){
+        for(let j = 0; j < squares; j++){
             const div = document.createElement('div');
             div.classList.add('element');
+            div.style.cssText = `height: ${squareSize}px; width: ${squareSize}px`
             grid.appendChild(div);
         }
     }
 }
 
-function paintElement(e){
-    this.style.cssText= 'border: none; background-color: black;'
-}
+//delete elements from grid 
+function deleteElementsFromGrid(){
+    const grid = document.querySelector('.grid');
 
-function getElementAndPaint(){
     const elements = document.querySelectorAll('.element');
-    elements.forEach(element => element.addEventListener('mouseover',paintElement))
+    elements.forEach(element => grid.removeChild(element));
+
 }
 
-function getGridSize(e){
+//get the element where the mouse is over and call paint function
+function getElementAndPaint(squares = 16){
+    const elements = document.querySelectorAll('.element');
+
+    elements.forEach(element => element.addEventListener('mouseover',e=>{
+        const grid = document.querySelector('.grid');
+
+        const squareSize = getSquareSize(grid, squares);
+        element.style.cssText= `border: none; background-color: black; height: ${squareSize}px; width: ${squareSize}px;`
+    }))
+}
+
+
+//this will set the grid size from initial 16 x 16 squares to numbers between 1 and 100
+function setGridSquares(e){
     do{
-        size = parseInt(prompt('Set the grid size n x n Between 1 and 100'));
-    }while(isNaN(size) || size > 100 || size < 1);
+        squares = parseInt(prompt('Set the grid size n x n Between 1 and 100'));
+    }while(isNaN(squares) || squares > 100 || squares < 1);
+
+    deleteElementsFromGrid();
+    setDivsGrid(squares);
+    getElementAndPaint(squares);
 }
 
 function getGridSizeByUser(){
     const gridSize = document.querySelector('.size');
     let size;
-    gridSize.addEventListener('click', getGridSize);
+    gridSize.addEventListener('click', setGridSquares);
 }
 
+
+//call methods to work
 setDivsGrid();
 
-getElementAndPaint();
-
 getGridSizeByUser();
+
+getElementAndPaint();
