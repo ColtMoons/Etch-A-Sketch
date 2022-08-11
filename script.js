97c0950d44e0ba83b990b2ba5511dad5831fc124
Coltmoons <coltmoonsac@gmail.com>
2022-08-10 22:47:24 -0500

    Add function to get size from user for grid

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function setDivsGrid(){
+function setDivsGrid(n = 16){
     const grid = document.querySelector('.grid');
 
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
             const div = document.createElement('div');
             div.classList.add('element');
             grid.appendChild(div);
@@ -19,7 +19,20 @@ function getElementAndPaint(){
     elements.forEach(element => element.addEventListener('mouseover',paintElement))
 }
 
+function getGridSize(e){
+    do{
+        size = parseInt(prompt('Set the grid size n x n Between 1 and 100'));
+    }while(isNaN(size) || size > 100 || size < 1);
+}
+
+function getGridSizeByUser(){
+    const gridSize = document.querySelector('.size');
+    let size;
+    gridSize.addEventListener('click', getGridSize);
+}
 
 setDivsGrid();
 
 getElementAndPaint();
+
+getGridSizeByUser();
