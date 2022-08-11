0a8f55e2adc3746ffc08ef455092dc205455efa8
Coltmoons <coltmoonsac@gmail.com>
2022-08-10 21:32:35 -0500

    Fix second for from i to j

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function setDivsGrid(){
     const grid = document.querySelector('.grid');
 
     for(let i = 0; i < 16; i++){
-        for(let i = 0; i < 16; i++){
+        for(let j = 0; j < 16; j++){
             const div = document.createElement('div');
             div.classList.add('element');
             grid.appendChild(div);
